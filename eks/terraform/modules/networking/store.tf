a512b9a38e13f9c3e77a647ebc6126f352b68711
michaelkedey <privatemyk@gmail.com>
2024-06-27 19:51:15 +0000

    eks terraform 3

--- eks/terraform/modules/networking/store.tf
+++ eks/terraform/modules/networking/store.tf
@@ -48,33 +48,3 @@ resource "aws_ssm_parameter" "sn_public2_id" {
 
 }
 
-# # resource "aws_ssm_parameter" "beanstalk_subnets" {
-# #   name = "/bid/beanstalk_subnets"
-# #   value = [
-# #     aws_subnet.sn_private1.id,
-# #     aws_subnet.sn_private2.id
-# #   ]
-# #   type = "String"
-
-# # }
-
-# resource "aws_ssm_parameter" "vpc_sg_id" {
-#   name  = "/bid/vpc_sg_id"
-#   type  = "String"
-#   value = aws_security_group.beanstalk_sg.id
-
-# }
-
-# resource "aws_ssm_parameter" "beanstalk_sn_lists" {
-#   name  = "/bid/beanstalk_sn_lists"
-#   type  = "String"
-#   value = join(",", [aws_subnet.sn_private1.id, aws_subnet.sn_private2.id])
-
-# }
-
-# resource "aws_ssm_parameter" "lb_sn_lists" {
-#   name  = "/bid/lb_sn_lists"
-#   type  = "String"
-#   value = join(",", [aws_subnet.sn_public1.id, aws_subnet.sn_public2.id])
-
-# }
